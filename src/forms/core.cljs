98fb399276a7ffa79026853303f1f4f17892559a
Mihael Konjevic <konjevic@gmail.com>
2016-04-12 08:00:15 +0200

    Simple validations work

--- src/forms/core.cljs
+++ src/forms/core.cljs
@@ -1,4 +1,26 @@
 (ns forms.core
-  (:require ))
+  (:require [clojure.string :as str]))
 
 (enable-console-print!)
+
+(defn ^:private validation-key-to-path [key]
+  (str/split (name key) "."))
+
+(defn ^:private validate-attr [value validators]
+  (reduce (fn [failed [name validator]]
+            (if (not (validator value))
+              (conj failed name)
+              failed)) [] validators))
+
+(defn ^:private validate-map [errors key attr-validators]
+  (let [path (validation-key-to-path key)
+        value (get-in input path)
+        failed (validate-attr value attr-validators)]
+    (if (pos? (count failed))
+      (assoc-in errors path {:value value :failed failed})
+      errors)))
+
+(defn validator [validators]
+  (fn [input]
+    (let [errors (reduce-kv validate-map {} validators)]
+      (if (= errors {}) nil errors))))
--- test/forms/test/core.cljs
+++ test/forms/test/core.cljs
@@ -1,1 +1,16 @@
-(ns forms.test.core)
+(ns forms.test.core
+  (:require [cljs.test :refer-macros [deftest is]]
+            [forms.core :as core]))
+
+(def not-nil [:not-nil (fn [a] (not (nil? a)))])
+(def is-one [:is-one (fn [a] (= 1 a))])
+
+(deftest simple-validator []
+  (let [validator (core/validator {:input [not-nil]})]
+    (is (= {"input" {:value nil :failed [:not-nil]}}
+           (validator {})))))
+
+(deftest multiple-validators []
+  (let [validator (core/validator {:input [not-nil is-one]})]
+    (is (= {"input" {:value nil :failed [:not-nil :is-one]}}
+           (validator {})))))
