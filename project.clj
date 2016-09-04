1c54a7b8cc73ce38cae2120143c0c1de47a1565a
Mihael Konjevic <konjevic@gmail.com>
2016-09-04 12:51:07 +0200

    Update Reagent version

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.7.228"]
                  [lein-doo "0.1.6"]
-                 [reagent "0.6.0-alpha"]
+                 [reagent "0.6.0-rc"]
                  [org.clojure/core.async "0.2.374"
                   :exclusions [org.clojure/tools.reader]]]
   
